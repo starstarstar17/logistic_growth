0ae9ae0ccfbed112a935d1f9b8d4a0f58509b0f2
Tara Karki <tara.karki@lmh.ox.ac.uk>
2024-10-30 12:36:06 +0000

    Logistic growth of E.coli, fitting linear model

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,6 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment.csv")
 
 logistic_fun <- function(t) {
   
@@ -10,18 +10,20 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
+N0 <- exp(6.889e+00)
   
-r <- ??? #
+r <- 1.002e-02
   
-K <- ??? #
+K <- 6.00e+10 
 
-ggplot(aes(???,???), data = growth_data) +
+
+ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
   geom_point()
 
-  #scale_y_continuous(trans='log10')
+scale_y_continuous(trans='log10')
+
 
 
